4673a37f6a08f1bfdbce88301a46659a4a52f11c
R. Josh Nylander <joshn@ridewta.com>
2015-12-01 10:22:22 -0800

    Updates

--- VMMPowerShellUtils.psm1
+++ VMMPowerShellUtils.psm1
@@ -2,13 +2,11 @@ function New-VMFromTemplate {
 	[CmdletBinding(SupportsShouldProcess=$true,DefaultParameterSetName="HostName")]
 	Param(
 		[Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true,HelpMessage="Name of the Virtual Machine to create.")]
-		[String]$NewVMName,
-    [Parameter(Mandatory=$false,Position=1,ValueFromPipeline=$true,HelpMessage="Template name")]
-		[String] $VMTemplateName,
-    [Parameter(Mandatory=$true,ParameterSetName="HostName",HelpMessage="VMHost to create VM on")]
-		[String]$VMHostName,
-		[Parameter(Mandatory=$false,HelpMessage="The name of a variable to place the job info into, see New-SCVirtualMachine")]
-		[String]$JobVariable
+		    [String]$NewVMName,
+    	[Parameter(Mandatory=$false,Position=1,ValueFromPipeline=$true,HelpMessage="Template name")]
+		    [String] $VMTemplateName,
+    	[Parameter(Mandatory=$true,ParameterSetName="HostName",HelpMessage="VMHost to create VM on")]
+		    [String]$VMHostName
 	)
 	Begin {
         #Put begining stuff here
@@ -23,26 +21,30 @@ function New-VMFromTemplate {
             }
         }
 
-				if (!$JobVariable) {
-					$JobVariable = "theJob"
-				}
+		$JobVariable = "theJob"
 
         $virtualMachineConfiguration = New-SCVMConfiguration -VMTemplate $VMTemplateName -Name $NewVMName
-        Write-Output $virtualMachineConfiguration
+
+        Write-Verbose $virtualMachineConfiguration
         $vmHost = Get-SCVMHost -ComputerName $VMHostName
+
         Set-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration -VMHost $vmHost -ComputerName $NewVMName
+        
         $VHDConfiguration = Get-SCVirtualHardDiskConfiguration -VMConfiguration $virtualMachineConfiguration
+        
         Set-SCVirtualHardDiskConfiguration -VHDConfiguration $VHDConfiguration -PinSourceLocation $false -PinDestinationLocation $false -FileName "$($NewVMName)_C.vhdx" -DeploymentOption "UseNetwork"
+        
         Update-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration
 
-        $vmConfig = New-SCVirtualMachine -Name $NewVMName -VMConfiguration $virtualMachineConfiguration -Description "" -BlockDynamicOptimization $false -StartVM -JobGroup $GUID -ReturnImmediately -StartAction "AlwaysAutoTurnOnVM" -StopAction "SaveVM" -JobVariable $JobVariable
+        New-SCVirtualMachine -Name $NewVMName -VMConfiguration $virtualMachineConfiguration -Description "" -BlockDynamicOptimization $false -StartVM -JobGroup $GUID -ReturnImmediately -StartAction "AlwaysAutoTurnOnVM" -StopAction "SaveVM" -JobVariable $JobVariable
+    
+    	#Set-SCVirtualMachine -VM $NewVMName -EnableTimeSync $false
+	
+        $result = New-Object -TypeName PSObject
+        Add-Member -InputObject $result -MemberType NoteProperty -Name Config -Value $virtualMachineConfiguration
+        Add-Member -InputObject $result -MemberType NoteProperty -Name Job -Value (Get-Variable -Name $JobVariable).Value
 
-				#If returning the JobVariable, create new variable with same name but in the parent scope
-				if ($JobVariable -ne "theJob") {
-					New-Variable -Name $JobVariable -Value (Get-Variable -Name $JobVariable) -Scope 1
-				}
-
-        return $vmConfig
+        return $result
 	}
 	End {
         #Put end here
@@ -72,7 +74,9 @@ function Add-VMHardDisk {
 
 		#Add the VHD to the VM
 		Add-VMHardDisk -VMName $VMName -Path $diskPath
-		#Mount, initalize, partition, and format the VHD from on the VM
+		
+        #Mount, initalize, partition, and format the VHD from on the VM
+        #TODO
 
 	}
 	End {
@@ -80,4 +84,38 @@ function Add-VMHardDisk {
 	}
 }
 
-Export-ModuleMember -Function "New-VMFromTemplate"
+<#
+.SYNOPSIS
+Rebuild VMTemplates based on Hardware Profiles and Guest OS Profiles in a consistent fasion.  This is used when "stand alone" profiles
+are updated.
+
+#>
+function Rebuild-VMTemplates {
+	[CmdletBinding(SupportsShouldProcess=$true)]
+	Param(
+		[Parameter(Mandatory=$true,HelpMessage="Provide the name of a VHD stored in the library")]
+		[Microsoft.SystemCenter.VirtualMachineManager.StandaloneVirtualHardDisk]$VHDObject
+	)
+	Begin {
+        $JobGUID = [System.Guid]::NewGuid()
+	}
+	Process {
+        forEach ($HWProfile in (Get-SCHardwareProfile)) {
+            forEach ($OSProfile in (Get-SCGuestOSProfile)) {
+                $TemplateName = $OSProfile.Name + "_" + $HWProfile.Name
+                Echo $TemplateName
+                if (Get-SCVMTemplate -Name $TemplateName) {
+                    #Delete first
+                    Get-SCVMTemplate -Name $TemplateName | Remove-SCVMTemplate -Force -RunAsynchronously
+                }
+                New-SCVMTemplate -VirtualHardDisk $VHDObject -Generation 1 -Name $TemplateName -HardwareProfile $HWProfile -GuestOSProfile $OSProfile -JobGroup $jobGUID
+            }
+        }
+	}
+	End {
+        #Put end here
+	}
+}
+
+
+Export-ModuleMember -Function "New-VMFromTemplate", "Rebuild-VMTemplates"
